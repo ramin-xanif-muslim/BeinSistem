a14284a3c04175a94ce2d664a559a0b77e3e7d45
ramin <raminxanifmuslim@gmail.com>
2021-12-15 09:27:04 +0400

    test6

--- src/RaminTest.js
+++ src/RaminTest.js
@@ -4,6 +4,8 @@ function RaminTest() {
 	return (
 		<div>
 			<h1>Testing git</h1>
+            <p>I need learn git to working in devoloper's comand.</p>
+            <p>cheincing both comand:mob app</p>
 		</div>
 	);
 }
